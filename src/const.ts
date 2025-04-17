e73c6bd49441e546bb527b4eea2581f91586979c
freddyskull <freddyskull11@gmail.com>
2025-04-17 19:19:08 -0400

    Add social networks and contact information constants

---
+++ src/const.ts
@@ -0,0 +1,26 @@
+export const social_networks = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/yourprofile",
+    icon: "facebook",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/yourprofile",
+    icon: "twitter",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/yourprofile",
+    icon: "linkedin",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/yourprofile",
+  },
+];
+export const contact = {
+  address: "0665 Broadway st. 10234 NY, USA",
+  email: "legal@lawdit.com",
+  phone: "+1 123 567 8910",
+};
