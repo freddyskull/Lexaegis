3956f7aca00fe4ca32b246371173900a02805c04
freddyskull <freddyskull11@gmail.com>
2025-04-17 21:28:41 -0400

    Agregar nuevos íconos SVG: actualizar constantes de redes sociales y contacto

--- src/const.ts
+++ src/const.ts
@@ -1,26 +1,44 @@
+import Email from "./components/svg/email.astro";
+import Facebook from "./components/svg/facebook.astro";
+import Instagram from "./components/svg/instagram.astro";
+import MapPin from "./components/svg/mapPin.astro";
+import Phone from "./components/svg/phone.astro";
+import Xsocial from "./components/svg/xsocial.astro";
 export const social_networks = [
   {
     name: "Facebook",
     url: "https://www.facebook.com/yourprofile",
-    icon: "facebook",
+    icon: Facebook,
   },
   {
     name: "Twitter",
     url: "https://twitter.com/yourprofile",
-    icon: "twitter",
+    icon: Instagram,
   },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/yourprofile",
-    icon: "linkedin",
-  },
-  {
-    name: "GitHub",
-    url: "https://github.com/yourprofile",
+    icon: Xsocial,
   },
 ];
-export const contact = {
-  address: "0665 Broadway st. 10234 NY, USA",
-  email: "legal@lawdit.com",
-  phone: "+1 123 567 8910",
-};
+export const contact = [
+  {
+    icon: MapPin,
+    title: "dirección",
+    label: "0665 Broadway st. 10234 NY, USA",
+    link: "https://www.google.com/maps/place/0665+Broadway+st.+10234+NY,+USA",
+  },
+
+  {
+    icon: Email,
+    title: "Correo",
+    label: "legal@lawdit.com",
+    link: "mailto:legal@lawdit.com",
+  },
+  {
+    icon: Phone,
+    title: "Teléfono",
+    label: "11-2356-789-10",
+    link: "tel:+11235678910",
+  },
+];
